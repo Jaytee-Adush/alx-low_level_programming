c82296c484b4fe2b20c136f6991fb816a6bafa82
Jaytee-Adush <julieabila@gmail.com>
2022-06-04 20:46:02 +0000

    print without printf and puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,10 @@
  * main - print function
  * Description: useful piece of art
  * Return: 1
- * /
- * int main(void)
- * {
- * write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
- * return (1)
- * }
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+return (1);
+}
+
